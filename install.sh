21d86196cf2ae4c0d5a82d41fa48d76099771a70
navajogit <146178899+navajogit@users.noreply.github.com>
2023-10-21 12:30:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ if ! type "aescrypt" &>/dev/null; then
     echo "Aescrypt is not installed. Installing..."
     cd ~/Downloads
     wget https://github.com/navajogit/aescrypt/raw/main/aescrypt-3.16.tgz &&
-#   curl -O https://www.aescrypt.com/download/v3/linux/aescrypt-3.16.tgz
     tar -xzf aescrypt-3.16.tgz &&
     cd aescrypt-3.16
     make &&
